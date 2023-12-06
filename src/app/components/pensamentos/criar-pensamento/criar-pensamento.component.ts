4eff0c3f1f9b7f670d434c2fd0935d5a9003bf3d
Knucklesx <gcfacchi@gmail.com>
2023-12-06 15:19:15 -0300

    Projeto 1 - site memórias

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1',
+  }
+  constructor(
+    private service:PensamentoService,
+    private router: Router,
+    ) {}
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => this.router.navigate(['/listarPensamento']))
+  }
+
+  cancelarPensamento() {
+    this.router.navigate(['/listarPensamento'])
+    }
+}
